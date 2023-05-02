d4c9abdbbdbc44551649725f4f5c28d76b5108bd
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2023-05-02 19:25:02 +0000

    1

---
+++ import_data.py
@@ -0,0 +1,65 @@
+import mysql.connector
+import pandas as pd
+
+#Connect to mysql
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+  database="CLASSIFICA_ANIMALI_ZOO"
+)
+mycursor = mydb.cursor()
+
+#Create the DB (if not already exists)
+mycursor.execute("CREATE DATABASE IF NOT EXISTS CLASSIFICA_ANIMALI_ZOO")
+
+#Create the table for the csv data (if not exists)
+mycursor.execute("""
+  CREATE TABLE IF NOT EXISTS CLASSIFICA_ANIMALI_ZOO.Animali (
+    animal_name VARCHAR(30) NOT NULL,
+    hair Boolean,
+    feathers Boolean,
+    eggs Boolean,
+    milk Boolean,
+    airborne Boolean,
+    aquatic Boolean,
+    predator Boolean,
+    toothed Boolean,
+    backbone Boolean,
+    breathes Boolean,
+    venomous Boolean,
+    fins Boolean,
+    legs Numeric,
+    tail Boolean,
+    domestic Boolean,
+    catsize Boolean,
+    class_type Numeric,
+    PRIMARY KEY (animal_name)
+  );""")
+
+#Delete data from the table Clsh_Unit
+mycursor.execute("DELETE FROM CLASSIFICA_ANIMALI_ZOO.Animali")
+mydb.commit()
+
+#Read data from a csv file
+Animali_data = pd.read_csv('./zoo.csv', index_col=False, delimiter = ',')
+Animali_data = Animali_data.fillna('Null')
+
+print(Animali_data.head(20))
+
+#Fill the table
+for i,row in Animali_data.iterrows():
+    cursor = mydb.cursor()
+    #here %S means string values 
+    sql = "INSERT INTO CLASSIFICA_ANIMALI_ZOO.Animali VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s%s,%s,%s,%s,%s,%s,%s,%s)"
+    cursor.execute(sql, tuple(row))
+    print("Record inserted")
+    # the connection is not auto committed by default, so we must commit to save our changes
+    mydb.commit()
+
+#Check if the table has been filled
+mycursor.execute("SELECT * FROM CLASSIFICA_ANIMALI_ZOO.Animali")
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
