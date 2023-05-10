fa8df69e557a7f4f709a15bb2bc9170feb12dedf
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2023-05-10 11:02:32 +0000

    4

--- import_data.py
+++ import_data.py
@@ -5,8 +5,7 @@ import pandas as pd
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="",
-  database="CLASSIFICA_ANIMALI_ZOO"
+  password=""
 )
 mycursor = mydb.cursor()
 
